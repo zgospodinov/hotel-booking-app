50648f1f3db1da418bc92d4e85c2a897b3e6af6c
zdravko.gospodinov <zdravko.gospodinov@gmail.com>
2025-06-24 12:24:27 +0300

    Implementation classes

--- main.py
+++ main.py
@@ -3,30 +3,47 @@ import os
 
 SCRIPT_DIR = os.path.dirname(os.path.abspath(__file__))
 
-df = pd.read_csv(f'{SCRIPT_DIR}/hotels.csv')
+df = pd.read_csv(f'{SCRIPT_DIR}/hotels.csv', dtype={'id': str, 'name': str, 'available': str})
 
 
 
 class Hotel:
     def __init__(self, hotel_id):
         self.hotel_id = hotel_id
-        self.hotel_data = df[df['id'] == hotel_id].iloc[0]
+        self.hotel_data = df.loc[df['id'] == hotel_id].squeeze() if not df.loc[df['id'] == hotel_id].empty else None
 
     def available(self):
         """Check if the hotel is available."""
-        pass
+
+        availability = self.hotel_data['available'] if self.hotel_data is not None else "no"
+        if availability.lower() == 'yes':
+            return True
+        else:
+            return False
         
     def book_room(self):
         """Book a room in the hotel."""
-        pass
+
+        df.loc[df['id'] == self.hotel_id, 'available'] = 'no'
+        df.to_csv(f'{SCRIPT_DIR}/hotels.csv', index=False)
+        print(f"Room booked successfully at {self.hotel_data['name']} hotel.")
     
 class ReservationTicket: 
     def __init__(self, name, hotel):
-        pass
+        self.customer_name = name
+        self.hotel = hotel
 
     def generate(self):
         """Generate a reservation ticket."""
-        pass
+        
+        content = f"""
+        Reservation Ticket
+        ------------------
+        Name: {self.customer_name}
+        Hotel: {self.hotel.hotel_data['name']}
+        Hotel ID: {self.hotel.hotel_id}
+        """
+        return content
     
 
 
@@ -35,8 +52,8 @@ id = input("Enter the hotel ID you want to book: ")
 hotel = Hotel(id)
 
 if hotel.available():
-    hotel.book_room()
     name = input("Enter your name: ")
+    hotel.book_room()
     reservation_ticket = ReservationTicket(name, hotel)
     print(reservation_ticket.generate())
 
