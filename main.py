01e6ecd2d325a474a64b3979242b24724577f172
zdravko.gospodinov <zdravko.gospodinov@gmail.com>
2025-06-24 08:51:12 +0300

    App flow

--- main.py
+++ main.py
@@ -1,6 +1,44 @@
-def main():
-    print("Hello from hotel-booking-app!")
+import pandas as pd
+import os
+
+SCRIPT_DIR = os.path.dirname(os.path.abspath(__file__))
+
+df = pd.read_csv(f'{SCRIPT_DIR}/hotels.csv')
 
 
-if __name__ == "__main__":
-    main()
+
+class Hotel:
+    def __init__(self, hotel_id):
+        self.hotel_id = hotel_id
+        self.hotel_data = df[df['id'] == hotel_id].iloc[0]
+
+    def available(self):
+        """Check if the hotel is available."""
+        pass
+        
+    def book_room(self):
+        """Book a room in the hotel."""
+        pass
+    
+class ReservationTicket: 
+    def __init__(self, name, hotel):
+        pass
+
+    def generate(self):
+        """Generate a reservation ticket."""
+        pass
+    
+
+
+print(df)
+id = input("Enter the hotel ID you want to book: ")
+hotel = Hotel(id)
+
+if hotel.available():
+    hotel.book_room()
+    name = input("Enter your name: ")
+    reservation_ticket = ReservationTicket(name, hotel)
+    print(reservation_ticket.generate())
+
+else:
+    print("Sorry, the hotel is not available for booking at the moment.")
